887f9375ab6ceed5b7b988f931ed90c5f756ba6d
emmarp <ruiizemmanuel@gmail.com>
2024-01-13 23:36:26 -0600

    Constructor Client modificado para omitir el ID

--- src/main/java/sample/taqueriadb/classes/Client.java
+++ src/main/java/sample/taqueriadb/classes/Client.java
@@ -13,6 +13,10 @@ public class Client {
         this.name = name;
     }
 
+    public Client(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
