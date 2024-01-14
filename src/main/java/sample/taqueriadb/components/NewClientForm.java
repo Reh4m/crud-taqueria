912d69e0218aab81607b3917f5f72892578ad3b0
emmarp <ruiizemmanuel@gmail.com>
2024-01-13 23:32:42 -0600

    Formulario agregar nuevo cliente

--- src/main/java/sample/taqueriadb/components/NewClientForm.java
+++ src/main/java/sample/taqueriadb/components/NewClientForm.java
@@ -1,12 +1,23 @@
 package sample.taqueriadb.components;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
 public class NewClientForm extends Stage {
     private Scene scene;
+    // Cuadrícula para ordenar los elementos del formulario.
+    private GridPane grid_pane_form;
+
+    // Entradas de texto.
+    private TextField name_input;
 
     public NewClientForm() {
         this.createUI();
@@ -16,13 +27,43 @@ public class NewClientForm extends Stage {
     }
 
     private void createUI() {
+        // Contenedor de los elementos del formulario.
+        grid_pane_form = new GridPane();
+        grid_pane_form.setAlignment(Pos.CENTER);
+        grid_pane_form.setHgap(10);
+        grid_pane_form.setVgap(10);
+        grid_pane_form.setPadding(new Insets(25, 25, 25, 25));
+
+        // Muestra el formulario para agregar un nuevo cliente.
+        showClientForm();
 
         //Layout principal.
         // Contiene únicamente el formulario.
         VBox container = new VBox();
+        container.getChildren().add(grid_pane_form);
         container.setSpacing(5);
         container.setPadding(new Insets(5));
 
+        // Ventana principal.
         scene = new Scene(container);
+    }
+
+    private void showClientForm() {
+        // Título.
+        Text title = new Text("Ingresa el nombre del nuevo cliente");
+        grid_pane_form.add(title, 0, 0, 2, 1);
+
+        // Nombre.
+        Label name_label = new Label("Nombre:");
+        grid_pane_form.add(name_label, 0, 1);
+
+        name_input = new TextField();
+        grid_pane_form.add(name_input, 1, 1);
+
+        // Botón para agregar un nuevo cliente.
+        Button btn_add_client = new Button("Agregar empleado");
+        btn_add_client.setMaxWidth(Double.MAX_VALUE);
+        btn_add_client.setOnAction(actionEvent -> {});
+        grid_pane_form.add(btn_add_client, 0, 2, 2, 1);
     }
 }
