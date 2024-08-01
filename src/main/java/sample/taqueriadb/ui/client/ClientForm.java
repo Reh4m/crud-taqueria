f9fc13f10ab2c8e07f6f77a611ec303c5198b434
emmarp <ruiizemmanuel@gmail.com>
2024-08-01 14:03:54 -0600

    Refactor: Implementación de clase abstracta en los formularios de datos

--- src/main/java/sample/taqueriadb/ui/client/ClientForm.java
+++ src/main/java/sample/taqueriadb/ui/client/ClientForm.java
@@ -1,16 +1,11 @@
 package sample.taqueriadb.ui.client;
 
 import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.scene.text.Text;
+import sample.taqueriadb.base.EditableForm;
 import sample.taqueriadb.model.Client;
 import sample.taqueriadb.dao.ClientDAO;
 
@@ -20,104 +15,25 @@ import java.util.concurrent.CompletableFuture;
 /**
  * Ventana que muestra un formulario para agregar o modificar un cliente en la base de datos.
  */
-public class ClientForm extends Stage {
-    private Scene scene;
-    // Cuadrícula para ordenar los elementos del formulario.
-    private GridPane grid_pane_form;
-
+public class ClientForm extends EditableForm<Client, ClientsList> {
     // Entradas de texto.
     private TextField name_input;
 
-    // Referencia a la clase ClientsList.
-    ClientsList clients_list;
-
-    // Datos del cliente a modificar
-    Client old_client;
-
-    /**
-     * Indica la forma en que se usará el formulario. Si es verdadero quiere decir que se agregará un nuevo cliente.
-     * Si el valor es falso entonces quiere decir que se modificará un cliente.
-     */
-    private final boolean is_new_client;
-
-    /**
-     * Crea una instancia para agregar un nuevo cliente.
-     *
-     * @param clients_list Referencia a la instancia de la clase ClientList para llamar a sus métodos internos e
-     *                     interactuar con la tabla de clientes.
-     */
     public ClientForm(ClientsList clients_list) {
-        // Instancia de la clase ClientsList para manejar la tabla de clientes.
-        this.clients_list = clients_list;
-
-        // Indica que se agregará un nuevo cliente.
-        is_new_client = true;
-
-        setupForm("Agregar nuevo cliente");
+        super("Agregar nuevo cliente", clients_list);
     }
 
-    /**
-     * Constructor secundario, encargado de recuperar los datos de un cliente en el caso de modificar sus atributos y
-     * posteriormente actualizarlos.
-     *
-     * @param clients_list Referencia a la instancia de la clase ClientList para llamar a sus métodos internos e
-     *                     interactuar con la tabla de clientes.
-     * @param old_client Objeto tipo Client con los datos del cliente a modificar.
-     */
     public ClientForm(ClientsList clients_list, Client old_client) {
-        // Instancia de la clase ClientList para actualizar la lista de clientes.
-        this.clients_list = clients_list;
-        // Instancia de la clase Client para recuperar los datos a actualizar.
-        this.old_client = old_client;
-
-        // Indica que se modificará un cliente.
-        is_new_client = false;
-
-        // Se crean los campos de texto antes de rellenarlos con los datos del cliente.
-        setupForm("Editar Cliente");
+        super("Editar Cliente", clients_list, old_client);
 
         // Se establecen los datos del cliente en sus respectivos campos de texto.
         name_input.setText(old_client.getName());
     }
 
     /**
-     * Establece la configuración básica de la ventana.
-     *
-     * @param window_title Nombre del título que tendrá la ventana.
-     */
-    private void setupForm(String window_title) {
-        createUI();
-        this.setTitle(window_title);
-        this.setScene(scene);
-        this.show();
-    }
-
-    private void createUI() {
-        // Contenedor de los elementos del formulario.
-        grid_pane_form = new GridPane();
-        grid_pane_form.setAlignment(Pos.CENTER);
-        grid_pane_form.setHgap(10);
-        grid_pane_form.setVgap(10);
-        grid_pane_form.setPadding(new Insets(25, 25, 25, 25));
-
-        showClientForm();
-
-        // Layout principal.
-        // Contiene únicamente el formulario.
-        VBox container = new VBox();
-        container.getChildren().add(grid_pane_form);
-        container.setSpacing(5);
-        container.setPadding(new Insets(5));
-
-        // Ventana principal.
-        scene = new Scene(container);
-    }
-
-    /**
      * Despliega los elementos del formulario para recibir los datos del cliente.
-     * Utiliza la clase TextField para recibir los datos por medio de entradas de texto.
      */
-    private void showClientForm() {
+    protected void displayFormElements() {
         // Título.
         Text title = new Text("Ingresa los datos del cliente");
         grid_pane_form.add(title, 0, 0, 2, 1);
@@ -132,77 +48,38 @@ public class ClientForm extends Stage {
         // Botón para agregar o actualizar un nuevo cliente.
         Button btn_add_client = new Button("Agregar");
         btn_add_client.setMaxWidth(Double.MAX_VALUE);
-        btn_add_client.setOnAction(actionEvent -> onAddButtonClicked());
+        btn_add_client.setOnAction(actionEvent -> onSaveButtonClicked());
         grid_pane_form.add(btn_add_client, 0, 2, 2, 1);
     }
 
     /**
-     * Indica al botón 'Agregar' qué acción realizar al momento de presionar el botón, esto dependiendo del uso que se
-     * le esté dando al formulario.
+     * Proporciona los datos ingresados en el formulario.
+     *
+     * @return objeto de tipo Client con los datos del client.
      */
-    private void onAddButtonClicked() {
-        if (is_new_client) {
-            addNewClient();
-        } else {
-            updateClient();
-        }
+    protected Client getFormData() {
+        return new Client(name_input.getText());
     }
 
     /**
-     * Agrega un nuevo cliente a la base de datos. Obtiene los datos ingresados en el formulario y hace un INSERT a la
-     * base de datos de manera asíncrona. Una vez terminado el proceso, se actualiza la tabla de clientes para mostrar
-     * al nuevo cliente agregado. Por último, se cierra la ventana actual.
+     * Maneja tanto la creación de un nuevo cliente como la actualización de uno existente.
+     * Obtiene los datos del formulario y realiza una operación INSERT o UPDATE en la base de datos de forma asíncrona.
+     * Tras ejecutar el proceso, actualiza la tabla de clientes y cierra la ventana.
      */
-    private void addNewClient() {
-        // Se obtienen los datos ingresados en el formulario.
-        Client new_client = new Client(name_input.getText());
+    protected void onSaveButtonClicked() {
+        // Obtiene los datos ingresados en el formulario.
+        Client new_client = getFormData();
 
-        // Ejecuta el proceso de manera asíncrona. Una vez finalizada la tarea, almacena el resultado del mismo y
-        // actualiza la tabla de clientes.
-        CompletableFuture.supplyAsync(() -> {
-            try {
-                return ClientDAO.add(new_client);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }).thenAccept(
-                // Este bloque de código se ejecuta en el hilo de la aplicación JavaFX, lo cual hace posible ejecutar
-                // operaciones en la interfaz de usuario (refreshTable, closeWindow).
-                rows_affected -> Platform.runLater(() -> {
-                    // Actualiza la lista de empleados en la interfaz.
-                    clients_list.refreshTable();
-
-                    System.out.println("Filas afectadas: " + rows_affected);
-
-                    // Cierra la ventana actual.
-                    this.close();
-                })
-        ).exceptionally(e -> {
-            e.printStackTrace();
-
-            return null;
-        });
-    }
-
-    /**
-     * Modifica un cliente existente en la base de datos. Obtiene los datos ingresados en el formulario y actualiza
-     * los datos del cliente (UPDATE) de manera asíncrona. Después de ejecutar el proceso, se actualiza la tabla de
-     * clientes y se cierra la ventana.
-     */
-    private void updateClient() {
-        // Se obtienen los datos ingresados en el formulario.
-        Client new_client = new Client(name_input.getText());
-
-        // Establece el ID del cliente.
+        // Establece el ID del cliente en caso de actualizar.
         // Este se obtiene desde la lista de clientes y lo asigna al objeto del cliente modificado.
         // El ID se utiliza dentro del query para saber qué cliente se va a actualizar.
-        new_client.setId(old_client.getId());
+        if (!is_new_item) new_client.setId(old_item.getId());
 
         // Ejecuta el proceso de manera asíncrona. Una vez finalizada la tarea, almacena el resultado del mismo y
         // actualiza la tabla de clientes.
         CompletableFuture.supplyAsync(() -> {
             try {
-                return ClientDAO.update(new_client);
+                return is_new_item ? ClientDAO.add(new_client) : ClientDAO.update(new_client);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -211,7 +88,7 @@ public class ClientForm extends Stage {
                 // operaciones en la interfaz de usuario (refreshTable, closeWindow).
                 rows_affected -> Platform.runLater(() -> {
                     // Actualiza la lista de clientes en la interfaz.
-                    clients_list.refreshTable();
+                    items_list.refreshTable();
 
                     System.out.println("Filas afectadas: " + rows_affected);
 
--- src/main/java/sample/taqueriadb/ui/employee/EmployeeForm.java
+++ src/main/java/sample/taqueriadb/ui/employee/EmployeeForm.java
@@ -1,16 +1,11 @@
 package sample.taqueriadb.ui.employee;
 
 import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import sample.taqueriadb.base.EditableForm;
 import sample.taqueriadb.model.Employee;
 import sample.taqueriadb.dao.EmployeeDAO;
 
@@ -20,64 +15,19 @@ import java.util.concurrent.CompletableFuture;
 /**
  * Ventana que muestra un formulario para agregar o modificar un empleado en la base de datos.
  */
-public class EmployeeForm extends Stage {
-    private Scene scene;
-    // Cuadrícula para ordenar los elementos del formulario.
-    private GridPane grid_pane_form;
-
+public class EmployeeForm extends EditableForm<Employee, EmployeesList> {
     // Entradas de texto.
     private TextField name_input;
     private TextField last_name_input;
     private TextField phone_number_input;
     private TextField email_input;
 
-    // Referencia a la clase EmployeesList.
-    EmployeesList employees_list;
-
-    // Datos del empleado a modificar.
-    Employee old_employee;
-
-    /**
-     * Indica la forma en que se usará el formulario. Si es verdadero quiere decir que se agregará un nuevo empleado.
-     * Si el valor es falso entonces quiere decir que se modificará un empleado.
-     */
-    private final boolean is_new_employee;
-
-    /**
-     * Crea una instancia para agregar un nuevo empleado.
-     *
-     * @param employees_list Referencia a la instancia de la clase EmployeesList para llamar a sus métodos internos
-     *                       e interactuar con la tabla de Empleados.
-     */
     public EmployeeForm(EmployeesList employees_list) {
-        // Instancia de la clase EmployeesList para actualizar la lista de empleados.
-        this.employees_list = employees_list;
-
-        // Indica que se agregará un nuevo empleado.
-        is_new_employee = true;
-
-        setupForm("Agregar nuevo empleado");
+        super("Agregar nuevo empleado", employees_list);
     }
 
-    /**
-     * Constructor secundario, encargado de recuperar los datos de un empleado en el caso de modificar sus atributos y
-     * posteriormente actualizarlos.
-     *
-     * @param employees_list Referencia a la instancia de la clase EmployeesList para llamar a sus métodos internos
-     *                       e interactuar con la tabla de empleados.
-     * @param old_employee Objeto tipo Employee con los datos del empleado a modificar.
-     */
     public EmployeeForm(EmployeesList employees_list, Employee old_employee) {
-        // Instancia de la clase EmployeesList para actualizar la lista de empleados.
-        this.employees_list = employees_list;
-        // Instancia del objeto Employee para recuperar los datos a actualizar.
-        this.old_employee = old_employee;
-
-        // Indica que se modificará un empleado.
-        is_new_employee = false;
-
-        // Se crean los campos de texto antes de rellenarlos con los datos del empleado.
-        setupForm("Editar Empleado");
+        super("Editar empleado", employees_list, old_employee);
 
         // Se establecen los datos del empleado en sus respectivos campos de texto.
         name_input.setText(old_employee.getName());
@@ -87,43 +37,9 @@ public class EmployeeForm extends Stage {
     }
 
     /**
-     * Establece la configuración básica de la ventana.
-     *
-     * @param window_title Nombre del título que tendrá la ventana.
-     */
-    private void setupForm(String window_title) {
-        createUI();
-        this.setTitle(window_title);
-        this.setScene(scene);
-        this.show();
-    }
-
-    private void createUI() {
-        // Contenedor de los elementos del formulario.
-        grid_pane_form = new GridPane();
-        grid_pane_form.setAlignment(Pos.CENTER);
-        grid_pane_form.setHgap(10);
-        grid_pane_form.setVgap(10);
-        grid_pane_form.setPadding(new Insets(25, 25, 25, 25));
-
-        showEmployeeForm();
-
-        // Layout principal.
-        // Contiene únicamente el formulario.
-        VBox container = new VBox();
-        container.getChildren().add(grid_pane_form);
-        container.setSpacing(5);
-        container.setPadding(new Insets(5));
-
-        // Ventana principal.
-        scene = new Scene(container);
-    }
-
-    /**
      * Despliega los elementos del formulario para recibir los datos del empleado.
-     * Hace uso de la clase TextField para recibir los datos por medio de una entrada de texto.
      */
-    private void showEmployeeForm() {
+    protected void displayFormElements() {
         // Título.
         Text title = new Text("Ingresa los datos del empleado");
         grid_pane_form.add(title, 0, 0, 2, 1);
@@ -159,28 +75,16 @@ public class EmployeeForm extends Stage {
         // Botón para agregar o actualizar un empleado.
         Button btn_add_employee = new Button("Agregar");
         btn_add_employee.setMaxWidth(Double.MAX_VALUE);
-        btn_add_employee.setOnAction(actionEvent -> onAddButtonClicked());
+        btn_add_employee.setOnAction(actionEvent -> onSaveButtonClicked());
         grid_pane_form.add(btn_add_employee, 0, 5, 2, 1);
     }
 
     /**
-     * Indica al botón 'Agregar' qué acción realizar al momento de presionar el botón, esto dependiendo del uso que se
-     * le esté dando al formulario.
-     */
-    private void onAddButtonClicked() {
-       if (is_new_employee) {
-           addNewEmployee();
-       } else {
-           updateEmployee();
-       }
-    }
-
-    /**
-     * Obtiene los datos ingresados en el formulario y los asigna a un objeto de tipo Employee.
+     * Proporciona los datos ingresados en el formulario.
      *
      * @return objeto de tipo Employee con los datos del empleado.
      */
-    private Employee getEmployeeData() {
+    protected Employee getFormData() {
         return new Employee(
             name_input.getText(),
             last_name_input.getText(),
@@ -190,60 +94,24 @@ public class EmployeeForm extends Stage {
     }
 
     /**
-     * Agrega un nuevo empleado a la base de datos. Obtiene los datos ingresados en el formulario y hace un INSERT a la
-     * base de datos de manera asíncrona. Una vez terminado el proceso, se actualiza la tabla de empleados para mostrar
-     * al nuevo empleado agregado. Por último, se cierra la ventana actual.
+     * Maneja tanto la creación de un nuevo empleado como la actualización de uno existente.
+     * Obtiene los datos del formulario y realiza una operación INSERT o UPDATE en la base de datos de forma asíncrona.
+     * Tras ejecutar el proceso, actualiza la tabla de empleados y cierra la ventana.
      */
-    private void addNewEmployee() {
-        // Se obtienen los datos ingresados en el formulario.
-        Employee new_employee = getEmployeeData();
+    protected void onSaveButtonClicked() {
+        // Obtiene los datos ingresados en el formulario.
+        Employee new_employee = getFormData();
 
-        // Ejecuta el proceso de manera asíncrona. Una vez finalizada la tarea, almacena el resultado del mismo y
-        // actualiza la tabla de empleados.
-        CompletableFuture.supplyAsync(() -> {
-            try {
-                return EmployeeDAO.add(new_employee);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }).thenAccept(
-            // Este bloque de código se ejecuta en el hilo de la aplicación JavaFX, lo cual hace posible ejecutar
-            // operaciones en la interfaz de usuario (refreshTable, closeWindow).
-            rows_affected -> Platform.runLater(() -> {
-                // Actualiza la lista de empleados en la interfaz.
-                employees_list.refreshTable();
-
-                System.out.println("Filas afectadas: " + rows_affected);
-
-                // Cierra la ventana actual.
-                this.close();
-            })
-        ).exceptionally(e -> {
-            e.printStackTrace();
-
-            return null;
-        });
-    }
-
-    /**
-     * Modifica un empleado existente en la base de datos. Obtiene los datos ingresados en el formulario y actualiza
-     * los datos del empleado (UPDATE) de manera asíncrona. Después de ejecutar el proceso, se actualiza la tabla de
-     * empleados y se cierra la ventana.
-     */
-    private void updateEmployee() {
-        // Se obtienen los datos ingresados en el formulario.
-        Employee new_employee = getEmployeeData();
-
-        // Establece el ID del empleado.
+        // Establece el ID del empleado en caso de actualizar.
         // Este se obtiene desde la lista de empleados y lo asigna al objeto del empleado modificado.
         // El ID se utiliza dentro del query para saber qué empleado se va a actualizar.
-        new_employee.setId(old_employee.getId());
+        if (!is_new_item) new_employee.setId(old_item.getId());
 
         // Ejecuta el proceso de manera asíncrona. Una vez finalizada la tarea, almacena el resultado del mismo y
         // actualiza la tabla de empleados.
         CompletableFuture.supplyAsync(() -> {
             try {
-                return EmployeeDAO.update(new_employee);
+                return is_new_item ? EmployeeDAO.add(new_employee) : EmployeeDAO.update(new_employee);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -252,7 +120,7 @@ public class EmployeeForm extends Stage {
             // operaciones en la interfaz de usuario (refreshTable, closeWindow).
             rows_affected -> Platform.runLater(() -> {
                 // Actualiza la lista de empleados en la interfaz.
-                employees_list.refreshTable();
+                items_list.refreshTable();
 
                 System.out.println("Filas afectadas: " + rows_affected);
 
